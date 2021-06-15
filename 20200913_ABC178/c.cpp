6aa7ffc7235204fa8682f65240e9eb835205bd53
kichico <suekiti3297@gmail.com>
2021-06-15 20:51:33 +0900

    solve ABC178,179

---
+++ 20200913_ABC178/c.cpp
@@ -0,0 +1,77 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+using ll=int64_t;
+using ld=long double;
+using ull=unsigned long long;
+template <class T>
+using grid=vector<vector<T>>;
+#define ALL(x) x.begin(),x.end()
+#define rep(iter,from,to) for(ll iter=from;iter<to;++iter)
+
+const ll MOD=1e9+7;
+const ll INF=1e17;
+//#######################################################################
+vector<vector<ll>> input(ll N, ll width){
+    string str;
+    vector<vector<ll>> vec(N,vector<ll>(width));
+    for(ll i=0;i<N;++i){
+        cin>>str;
+        reverse(ALL(str));
+        for(ll j=0;j<width;++j){
+            vec[i][j]=str.back();
+            str.pop_back();
+        }
+    }
+    return vec;
+}
+void op(vector<ll> vec){
+    ll size=(ll)vec.size();
+    for(ll i=0;i<size-1;++i) cout<<vec[i]<<" ";
+    cout<<vec.back()<<endl;
+}
+
+void op(vector<vector<ll>> vec){
+    ll height=(ll)vec.size();
+    ll width=(ll)vec[0].size();
+    for(ll i=0;i<height;++i) {
+        for(ll j=0;j<width-1;++j) cout<<vec[i][j]<<" ";
+        cout<<vec[i].back()<<endl;
+    }
+}
+//########################################################################
+ll lim=3000001;
+vector<ll> fac_n(lim);
+vector<ll> fac_k(lim);
+
+ll modpow(ll x, ll n){
+    ll ans=1;
+    while(n>0) {
+        if(n&1) ans=ans*x%MOD;
+        x=x*x%MOD;
+        n>>=1;
+    }
+    return ans;
+}
+
+ll com(ll n, ll k, vector<ll> fac_n,vector<ll> fac_k){
+    if(n==0&&k==0) return 1;
+    if(n<k||n<0) return 0;
+    ll value=fac_k[n-k]*fac_k[k]%MOD;
+    return value*fac_n[n]%MOD;
+}
+
+void solve(){
+    ll N;
+    cin>>N;
+    ll sum=1;
+    sum=modpow(10,N)-(modpow(9,N)*2-modpow(8,N));
+    cout<<sum<<endl;
+}
+
+
+int main(void){
+    std::cin.tie(nullptr);
+	std::ios_base::sync_with_stdio(false);
+	std::cout << std::fixed << std::setprecision(15);
+	solve();
+}
---
+++ 20200919_ABC179/b.cpp
@@ -0,0 +1,69 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+using ll=int64_t;
+using ld=long double;
+using ull=unsigned long long;
+template <class T>
+using grid=vector<vector<T>>;
+#define ALL(x) x.begin(),x.end()
+#define rep(iter,from,to) for(ll iter=from;iter<to;++iter)
+
+const ll MOD=1e9+7;
+const ll INF=1e17;
+//#######################################################################
+vector<vector<ll>> input(ll N, ll width){
+    string str;
+    vector<vector<ll>> vec(N,vector<ll>(width));
+    for(ll i=0;i<N;++i){
+        cin>>str;
+        reverse(ALL(str));
+        for(ll j=0;j<width;++j){
+            vec[i][j]=str.back();
+            str.pop_back();
+        }
+    }
+    return vec;
+}
+void op(vector<ll> vec){
+    ll size=(ll)vec.size();
+    for(ll i=0;i<size-1;++i) cout<<vec[i]<<" ";
+    cout<<vec.back()<<endl;
+}
+
+void op(vector<vector<ll>> vec){
+    ll height=(ll)vec.size();
+    ll width=(ll)vec[0].size();
+    for(ll i=0;i<height;++i) {
+        for(ll j=0;j<width-1;++j) cout<<vec[i][j]<<" ";
+        cout<<vec[i].back()<<endl;
+    }
+}
+//########################################################################
+
+
+
+
+
+void solve(){
+    ll N;
+    cin>>N;
+    vector<pair<ll,ll>> dice(N);
+    rep(i,0,N) cin>>dice[i].first>>dice[i].second;
+    bool flg=false;
+    rep(i,0,N-2) {
+        if(dice[i].first!=dice[i].second) continue;
+        if(dice[i+1].first!=dice[i+1].second) continue;
+        if(dice[i+2].first!=dice[i+2].second) continue;   
+        flg=true;
+    }
+    if(flg) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+
+int main(void){
+    std::cin.tie(nullptr);
+	std::ios_base::sync_with_stdio(false);
+	std::cout << std::fixed << std::setprecision(15);
+	solve();
+}
