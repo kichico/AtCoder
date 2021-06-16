a4bf71055c4fc17e198afd51d21264edd0500c07
kichico <suekiti3297@gmail.com>
2021-06-16 17:02:22 +0900

    solved some problem

---
+++ Practice/ABC150B.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+using ll=long long;
+
+int main(){
+  ll N;
+  cin>>N;
+  string str,s;
+  cin>>str;
+  reverse(str.begin(),str.end());
+  ll cnt=0;
+  while(!str.empty()){
+    s.push_back(str.back());
+    str.pop_back();
+    if(s.size()>=3){
+      string ss;
+      ss.push_back(s[s.size()-3]);
+      ss.push_back(s[s.size()-2]);
+      ss.push_back(s[s.size()-1]);
+      //cout<<"ss:"<<ss<<endl;
+      if(ss=="ABC") cnt++;
+    }
+  }
+  cout<<cnt<<endl;
+  return 0;
+}
---
+++ Practice/CODEFESTIVAL2014B.cpp
@@ -0,0 +1,70 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+using ll=int64_t;
+using ld=long double;
+using ull=unsigned long long;
+template <class T>
+using grid=vector<vector<T>>;
+#define ALL(x) x.begin(),x.end()
+#define rep(iter,from,to) for(ll iter=from;iter<to;++iter)
+
+const ll MOD=1e9+7;
+const ll INF=1e17;
+//#######################################################################
+vector<vector<ll>> input(ll N, ll width){
+    string str;
+    vector<vector<ll>> vec(N,vector<ll>(width));
+    for(ll i=0;i<N;++i){
+        cin>>str;
+        reverse(ALL(str));
+        for(ll j=0;j<width;++j){
+            vec[i][j]=str.back();
+            str.pop_back();
+        }
+    }
+    return vec;
+}
+void op(vector<ll> vec){
+    ll size=(ll)vec.size();
+    for(ll i=0;i<size-1;++i) cout<<vec[i]<<" ";
+    cout<<vec.back()<<endl;
+}
+
+void op(vector<vector<ll>> vec){
+    ll height=(ll)vec.size();
+    ll width=(ll)vec[0].size();
+    for(ll i=0;i<height;++i) {
+        for(ll j=0;j<width-1;++j) cout<<vec[i][j]<<" ";
+        cout<<vec[i].back()<<endl;
+    }
+}
+//########################################################################
+
+
+
+
+
+void solve(){
+    string str;
+    cin>>str;
+    ll N;
+    cin>>N;
+    ll len=str.size();
+    ll pos=N%len;
+    //cout<<pos<<endl;
+    if(pos!=0) {
+        pos--;
+        cout<<str[pos]<<endl;
+    }
+    else{
+        cout<<str.back()<<endl;
+    }
+}
+
+
+int main(void){
+    std::cin.tie(nullptr);
+	std::ios_base::sync_with_stdio(false);
+	std::cout << std::fixed << std::setprecision(15);
+	solve();
+}
