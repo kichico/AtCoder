98ca59a585bf85170f73249530c200e0f72a226d
kichico <suekiti3297@gmail.com>
2021-06-25 15:49:20 +0900

    solved some problem

---
+++ 20200531_ABC169/b.py
@@ -0,0 +1,15 @@
+﻿N=int(input())
+array=list(map(int, input().strip().split()))
+ans=1
+#print(array)
+if 0 in array:
+    print(0)
+    exit()
+
+for i in range(len(array)):
+    ans=ans*array[i]
+    if ans > 1e18:
+        print(-1)
+        exit()
+
+print(ans)
