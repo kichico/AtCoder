8d45eb523e09ea2f0ebbbe925b116fa8c58bc6d8
kichico <suekiti3297@gmail.com>
2021-12-08 22:22:49 +0900

    solved someproblem

---
+++ 20200829_ABC177rust/a.rs
@@ -0,0 +1,16 @@
+﻿use proconio::input;
+
+fn solve() {
+    input! {
+        _n:i32,
+        x:i32,
+        t:i32,
+    }
+    let mut N = _n;
+    let mut ans: i32 = 0;
+    while N > 0 {
+        N -= x;
+        ans += t;
+    }
+    println!("{}", ans);
+}
---
+++ 20200829_ABC177rust/c.rs
@@ -0,0 +1,17 @@
+﻿fn solve() {
+    input! {
+        n:usize,
+        mut vec:[i64;n],
+    }
+    let mut ans: i64 = 0;
+    let mut high: i64 = vec[0];
+    for i in 1..n {
+        high = std::cmp::max(high, vec[i]);
+        if high > vec[i] {
+            ans += high - vec[i];
+            vec[i] = high;
+        }
+        //println! {"high:{}",high};
+    }
+    println!("{}", ans);
+}
